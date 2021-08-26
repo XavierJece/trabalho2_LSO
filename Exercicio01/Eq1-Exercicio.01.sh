a3e383775f35879edfe18be5112124609e2ddb0f
Jece Xavier <jece.pereira@etec.sp.gov.br>
2021-08-26 19:46:41 -0300

    1.3 question resolved

--- Exercicio01/Eq1-Exercicio.01.sh
+++ Exercicio01/Eq1-Exercicio.01.sh
@@ -18,7 +18,7 @@
 # 0 - Contruir uma variável que receba o nome completo de todos os alunos;                 |
 # 1 - Construa uma identificação dos Alunos com Nome Completo;                             |
 # 2 - Construa uma identificação do Curso que está cursando;                               |
-# 3 - Construa uma variável que recebe o nome completo do Diretório Corrente (Pasta atual);| usar pwd, funciona no power shell?
+# 3 - Construa uma variável que recebe o nome completo do Diretório Corrente (Pasta atual);| 
 # 4 - Construa um Arquivo denominado Base_Dados.csv, coletando uma listagem completa do    |
 # diretório "/etc" ordenado por Data; OBSERVAÇÃO: Sem sair do diretório corrente           |
 # 5 - Extrair do Arquivo gerado as seguintes informações:                                  |
@@ -55,6 +55,9 @@ done
 #===================================== Exercicio 1.2 ===================================== +
 
 #===================================== Exercicio 1.3 ===================================== +
+CURRENT_DIRETORY=$(pwd)
+
+echo $CURRENT_DIRETORY
 
 #===================================== Exercicio 1.4 ===================================== +
 LIST_DIRECTORIES_ETC=$(ls -dtlr /etc/*/ | awk '{printf "%s,%s,%s,%s\r", $6, $7, $8, $9}')
