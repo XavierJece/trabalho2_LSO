fb21a6d56700953134cef4551fd17beeccb2ccb1
Jece Xavier <jece.pereira@etec.sp.gov.br>
2021-08-26 19:43:30 -0300

    1.4 question resolved

--- Exercicio01/Eq1-Exercicio.01.sh
+++ Exercicio01/Eq1-Exercicio.01.sh
@@ -18,7 +18,7 @@
 # 0 - Contruir uma variável que receba o nome completo de todos os alunos;                 |
 # 1 - Construa uma identificação dos Alunos com Nome Completo;                             |
 # 2 - Construa uma identificação do Curso que está cursando;                               |
-# 3 - Construa uma variável que recebe o nome completo do Diretório Corrente (Pasta atual);|
+# 3 - Construa uma variável que recebe o nome completo do Diretório Corrente (Pasta atual);| usar pwd, funciona no power shell?
 # 4 - Construa um Arquivo denominado Base_Dados.csv, coletando uma listagem completa do    |
 # diretório "/etc" ordenado por Data; OBSERVAÇÃO: Sem sair do diretório corrente           |
 # 5 - Extrair do Arquivo gerado as seguintes informações:                                  |
@@ -39,7 +39,7 @@
 #===================================== Exercicio 1.0 ===================================== +
 MEMBERS=("Danrlei Vieira da Silva de Souza" "Jean Carlos da Silva" "Jecé Xavier Pereira Neto" "Vagner Humberto Wentz")
 
-echo "MEMBERS: ${MEMBERS[@]}"
+# echo "MEMBERS: ${MEMBERS[@]}"
 
 #===================================== Exercicio 1.1 ===================================== +
 declare -a MEMBERS_ID
@@ -50,12 +50,15 @@ for ((i=0; i<${#MEMBERS[@]}; i++)); do
   MEMBERS_ID+=("$MEMBER_NO_WHITESPACE")
 done
 
-echo "MEMBERS_ID: ${MEMBERS_ID[@]}"
+# echo "MEMBERS_ID: ${MEMBERS_ID[@]}"
 
 #===================================== Exercicio 1.2 ===================================== +
 
 #===================================== Exercicio 1.3 ===================================== +
 
 #===================================== Exercicio 1.4 ===================================== +
+LIST_DIRECTORIES_ETC=$(ls -dtlr /etc/*/ | awk '{printf "%s,%s,%s,%s\r", $6, $7, $8, $9}')
+echo $LIST_DIRECTORIES_ETC > ./Base_Dados.csv
+# echo $LIST_DIRECTORIES_ETC
 
 #===================================== Exercicio 1.5 ===================================== +
